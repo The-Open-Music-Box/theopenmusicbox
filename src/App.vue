12e5b1976242d5012b47b4fe8c8d0a4d3daa5ff0
Jonathan Piette <jo.piette@me.com>
2025-03-19 14:05:42 +0100

    fix color import

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
     </div>
     <button
       @click="sendMessage"
-      :class="[colors.primary.main, 'mt-4 px-4 py-2 text-white rounded hover:' + colors.background.primary.main]"
+      :class="[colors.primary.main, 'mt-4 px-4 py-2 text-white rounded hover:' + colors.primary.hover]"
     >
       {{ $t('common.sendMessage') }}
     </button>
