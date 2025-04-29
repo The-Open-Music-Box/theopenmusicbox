ff7e8d7c16b5d2bf977359a3981bb3a869640887
Jonathan Piette <jo.piette@me.com>
2025-04-29 09:53:02 +0200

    fix nfc errors on run

--- app/src/module/nfc/nfc_handler.py
+++ app/src/module/nfc/nfc_handler.py
@@ -1,4 +1,5 @@
-# app/src/module/nfc/nfc_handler.py
+# back/app/src/module/nfc/nfc_handler.py
+
 from typing import Generic, TypeVar
 from .nfc_hardware import NFCHardware
 
@@ -7,6 +8,24 @@ T = TypeVar('T', bound=NFCHardware)
 class NFCHandler(Generic[T]):
     def __init__(self, hardware: T):
         self._hardware = hardware
+
+    @property
+    def tag_subject(self):
+        if hasattr(self._hardware, "tag_subject"):
+            return self._hardware.tag_subject
+        raise AttributeError("Underlying hardware does not have a tag_subject property.")
+
+    def start_nfc_reader(self) -> None:
+        if hasattr(self._hardware, "start_nfc_reader"):
+            self._hardware.start_nfc_reader()
+        else:
+            raise NotImplementedError("Underlying hardware does not support start_nfc_reader().")
+
+    def stop_nfc_reader(self) -> None:
+        if hasattr(self._hardware, "stop_nfc_reader"):
+            self._hardware.stop_nfc_reader()
+        else:
+            raise NotImplementedError("Underlying hardware does not support stop_nfc_reader().")
 
     def read_tag(self) -> str:
         return self._hardware.read_tag()
